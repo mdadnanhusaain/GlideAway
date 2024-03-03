ba5c1f1c4359291502680a3a3925e715e4ca3726
Md Adnan Hussain <mdadnanhusaain@gmail.com>
2024-03-03 13:55:10 +0530

    Modulated Code and added default map location for empty fields

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,17 @@
 mapboxgl.accessToken = mapToken;
+
+let coordinates = listing.geometry.coordinates;
+if (coordinates.length == 0) coordinates = [77.209006, 28.613895];
+console.log(coordinates);
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v12", // style URL
-  center: listing.geometry.coordinates, // starting position [lng, lat]
+  center: coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
 
 const marker1 = new mapboxgl.Marker({ color: "red" })
-  .setLngLat(listing.geometry.coordinates) // Listings.geometry.coordinates
+  .setLngLat(coordinates) // Listings.geometry.coordinates
   .setPopup(
     new mapboxgl.Popup({ offset: 25 })
       .setHTML(
